e84dfdf7675329053af5d031376301ade85ca916
2dayistheday <kmk3075@gmail.com>
2017-10-02 05:23:06 +0900

    BOJ 11650

--- beakjun/src/beakjun/p_11650.java
+++ beakjun/src/beakjun/p_11650.java
@@ -1,45 +1,46 @@
 package beakjun;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class p_11650 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		int arr[][] = new int[t][2];
-		int sort[][] = new int[t][2];
+		pair arr[] = new pair[t];
 		
 		for(int i = 0; i < t; i++) {
-			for(int j = 0; j <2; j++) {
-				int n = sc.nextInt();
-				arr[i][j] = n;
-				
-				int prio = 1;
-				for(int e = 0; e < i; e++) {
-					if(arr[e][j] < n) {
-						prio++;
-					}else if(arr[e][j] == n){
-						
-					}else {
-						sort[e][j]++;
-					}
-				}
-				sort[i][j] = prio;
-			}
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			arr[i] = new pair(x, y);
 		}
-		
-		for(int i = 0; i < t; i++) {
-			int minIdx = 0;
-			for(int j = 1; j < t; j++) {
-				if(sort[minIdx][0] > sort[j][0]) {
-					minIdx = j;
-				}else if(sort[minIdx][0] == sort[j][0]){
-					minIdx = (sort[minIdx][1] < sort[j][1])? minIdx : j;
+		Arrays.sort(arr, new Comparator<pair>() {
+			@Override
+			public int compare(pair a, pair b) {
+				int x1 = a.x;
+				int x2 = b.x;
+				if(x1 == x2) {
+					int y1 = a.y;
+					int y2 = b.y;
+					
+					return y1-y2;
+				}else {
+					return x1-x2;
 				}
 			}
-			System.out.println(arr[minIdx][0] + " "+arr[minIdx][1]);
-			sort[minIdx][0] = 100001;
-			sort[minIdx][1] = 100001;
-		}//시간초과....
+		});
+		for(int j = 0; j < t; j++) {
+			System.out.println(arr[j].x + " " + arr[j].y);
+		}
+	}
+}
+class pair{
+	int x;
+	int y;
+	public pair(int x, int y) {
+		this.x =x;
+		this.y = y;
 	}
 }
